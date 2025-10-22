c78107e8a13927013d9bc36d9217e86b095e200d
John Masseria <jmasseri@mdc.edu>
2025-10-22 17:26:30 -0400

    Change animation to create an additional box around the initial product of the functions

--- main.py
+++ main.py
@@ -7,14 +7,18 @@ class MovingFrameBox(Scene):
             "g(x)\\frac{d}{dx}f(x)"
         )
         self.play(Write(text))
+        framebox0 = SurroundingRectangle(text[0], buff = .1)
         framebox1 = SurroundingRectangle(text[1], buff = .1)
         framebox2 = SurroundingRectangle(text[3], buff = .1)
         self.play(
-            Create(framebox1),
+            Create(framebox0),
+        )
+        self.wait()
+        self.play(
+            ReplacementTransform(framebox0,framebox1),
         )
         self.wait()
         self.play(
             ReplacementTransform(framebox1,framebox2),
         )
         self.wait()
-
